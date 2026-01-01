132026c7e589057f690ca9bf658335d44e1bccc6
Sayem2004 <mdalimransayem@gmail.com>
2026-01-01 16:17:52 +0600

    Basic php

---
+++ PHP Practices/basic.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>This is My First PHP Page</h1>
+    <?php
+    // single line comment
+    # Also single line comment
+    /* This is 
+    miulitpe 
+    line comment
+    */
+    echo"Good Night"
+    ?>
+</body>
+</html>
