dcc3312d426a5639001b604a7b2c11b84638afb8
Sayem2004 <mdalimransayem@gmail.com>
2026-01-01 16:40:03 +0600

    Variables

---
+++ PHP Practices/printing.php
@@ -0,0 +1,19 @@
+<?php
+$txt1 ="Good Morning";
+$txt2 = "Night";
+echo" <h2>$txt1</h2>";
+echo" <p> Study Php at $txt2 </p>";
+echo '<h2>' . $txt1 . '</h2>';
+
+echo '<h1> Study php at ' .$txt2. '<h1>';
+
+
+?>
+<?php
+$txt ="Hellow World     ";
+$a =10.5;
+$y =10.5;
+echo "==== $txt" .$a. "!";
+echo $a + $y;
+
+?>
