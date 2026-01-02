60cf5ca421c83fce89482fb5fd3cdc0b44d26577
Sayem2004 <mdalimransayem@gmail.com>
2026-01-02 17:09:27 +0600

    Basic php

--- PHP Practices/printing.php
+++ PHP Practices/printing.php
@@ -5,7 +5,7 @@ echo" <h2>$txt1</h2>";
 echo" <p> Study Php at $txt2 </p>";
 echo '<h2>' . $txt1 . '</h2>';
 
-echo '<h1> Study php at ' .$txt2. '<h1>';
+echo '<h1> Study php at ' .$txt2. '</h1>';
 
 
 ?>
@@ -13,7 +13,49 @@ echo '<h1> Study php at ' .$txt2. '<h1>';
 $txt ="Hellow World     ";
 $a =10.5;
 $y =10.5;
-echo "==== $txt" .$a. "!";
-echo $a + $y;
+echo "==== $txt" .$a. "! <br>";
+echo $a + $y ."<br>";
 
+?>
+
+<?php
+$cars = array("Honda", "BMW" ,"Toyota", "OOOOD");
+var_dump($cars);
+?>
+
+<?php
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+
+echo $car["model"]; 
+
+
+foreach ($car as $x => $y) {
+  echo " <br> $x: $y <br>";
+}
+?>
+
+<?php
+class Car {
+  public $model = "VW <br>";
+}
+
+$herbie = new Car();
+
+echo $herbie->model;
+?>
+
+<?php
+
+
+for($x=0; $x <= 10 ; $x++){
+    echo "The nunber is: $x <br>"; 
+}
+
+?>
+<?php
+$colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value) {
+  echo "$value <br>";
+}
 ?>
