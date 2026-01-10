7facc7b50b43e1bb11e3cf0ad70e0a256fb184cc
Sayem2004 <mdalimransayem@gmail.com>
2026-01-10 22:29:16 +0600

    registation Form For practices

---
+++ PHP Practices/reg.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Registration</title>
+</head>
+<body>
+
+<h2>User Registration Form</h2>
+
+<form method="post" action="register_validation.php">
+
+    <label>Name:</label><br>
+    <input type="text" name="name"><br><br>
+
+    <label>Email:</label><br>
+    <input type="email" name="email"><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password"><br><br>
+
+    <label>Confirm Password:</label><br>
+    <input type="password" name="confirm_password"><br><br>
+
+    <input type="submit" value="Register">
+
+</form>
+
+</body>
+</html>
